a306560113ae591ff65621545aeebfb5a817ce6a
ruosen.chai <chairuosen@gmail.com>
2017-02-13 11:37:21 +0800

    first

---
+++ src/components/test3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="active">
+      3333
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        active:true
+      }
+    },
+    methods:{
+      show:function () {
+        this.active = true;
+      }
+    }
+  }
+</script>
